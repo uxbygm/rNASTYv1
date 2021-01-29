4cae92f978cd7920f33598350ffecf6c7f9ac90f
Gabriel Meacham <gabe@uxbygm.com>
2021-01-28 23:29:28 -0500

    Added JigSaw Storybook Theme

--- .storybook/JigsawTheme.js
+++ .storybook/JigsawTheme.js
@@ -1,8 +1,9 @@
 import { create } from "@storybook/theming/create";
 
 export default create({
-  base: "light",
+  base: "dark",
+  appBg: "#232323",
   brandTitle: "Jigsaw rNastyv1 Atomic Design Boilerplate",
   brandUrl: "https://github.com/uxbygm/rNASTYv1",
-  brandImage: "rnasty-logo-storybook.png",
+  brandImage: "http://localhost:3000/rnasty-logo-storybook-dark.png",
 });
